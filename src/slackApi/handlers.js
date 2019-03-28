bf8776fae962e72a756db9a87d490af57a6d4bdd
AleksandrZhukov <aleksandr.a.zhukov@gmail.com>
2019-03-28 14:50:08 +0200

    refactored btn hadnlers, added deleting sticker

--- src/slackApi/handlers.js
+++ src/slackApi/handlers.js
@@ -3,12 +3,15 @@ const util = require('util');
 const db = require('../db');
 const config = require('dotenv').config().parsed;
 const md5 = require('md5');
+const _ = require('lodash');
 const URL = config.URL;
 
 const readdirAsync = util.promisify(fs.readdir);
 
 const randomKey = 'XPFG';
 const stickersPerPage = 2;
+const PRIVATE = 'ephemeral';
+const PUBLIC = 'in_channel';
 
 const getAttachments = async (userId, page = 0) => {
   const token = db.get(`tokens.${md5(userId)}`).value();
@@ -30,6 +33,13 @@ const getAttachments = async (userId, page = 0) => {
           text: 'Select',
           type: 'button',
           value: f
+        },
+        {
+          name: 'remove_sticker',
+          text: 'Remove',
+          type: 'button',
+          style: 'danger',
+          value: `remove_${f}_page${page}`
         }
       ]
     }))
@@ -37,23 +47,22 @@ const getAttachments = async (userId, page = 0) => {
 
   const navigationBtns = [
     page > 0 && {
-      name: 'send_sticker',
+      name: 'show_page',
       text: 'Prev',
       type: 'button',
-      value: `nextPage_${page - 1}`
+      value: page - 1
     },
     userImages.length - ((page + 1) * stickersPerPage) > 0 && {
-      name: 'send_sticker',
+      name: 'show_page',
       text: 'Next',
       type: 'button',
-      value: `nextPage_${page + 1}`
+      value: page + 1
     },
     {
-      name: 'send_sticker',
+      name: 'cancel',
       text: 'Cancel',
       type: 'button',
-      style: 'danger',
-      value: 'cancel'
+      style: 'danger'
     }
   ];
 
@@ -73,7 +82,7 @@ const getStickers = async function(req, res, next) {
     const token = req.body.text.replace(/(^XPFG|\s)/g, '');
     db.update('tokens', tokens => ({ ...tokens, [md5(userId)]: token })).write();
     return res.json({
-      response_type: 'ephemeral', // private message
+      response_type: PRIVATE,
       text: 'You are registered, now you can send stickers'
     });
   }
@@ -81,38 +90,20 @@ const getStickers = async function(req, res, next) {
   const attachments = await getAttachments(userId);
 
   res.json({
-    response_type: 'ephemeral', // private message
+    response_type: PRIVATE,
     text: 'Select sticker',
     attachments
   });
 };
 
-const handleBtn = async function(req, res, next) {
-  const payload = JSON.parse(req.body.payload);
+const sendSticker = (payload, res) => {
   const value = payload.actions[0].value;
-  const userId = payload.user.id;
   const userName = payload.user.name;
 
-  if (value === 'cancel') {
-    return res.json({ delete_original: true });
-  }
-  if (value.includes('nextPage_')) {
-    const attachments = await getAttachments(
-      userId,
-      +value.replace('nextPage_', '')
-    );
-
-    return res.json({
-      replace_original: true,
-      response_type: 'ephemeral', // private message
-      text: 'Select sticker',
-      attachments
-    });
-  }
-  res.json({
+  return res.json({
     delete_original: true,
     replace_original: false,
-    response_type: 'in_channel', // public to the channel
+    response_type: PUBLIC,
     attachments: [
       {
         color: '#7CD197',
@@ -122,6 +113,51 @@ const handleBtn = async function(req, res, next) {
       }
     ]
   });
+};
+
+const removeSticker = async (payload, res) => {
+  const value = payload.actions[0].value;
+  const userId = payload.user.id;
+
+  const [_, attachmentId, page] = value.match(/remove_(.+).png_page(\d)/);
+  const token = db.get(`tokens.${md5(userId)}`).value();
+  db.get(`userImages.${token}`).pull(attachmentId).write();
+
+  const attachments = await getAttachments(userId, +page);
+
+  return res.json({
+    replace_original: true,
+    response_type: PRIVATE,
+    text: 'Select sticker',
+    attachments
+  });
+};
+
+const cancelRequest = (res) => res.json({ delete_original: true });
+
+const showStickersPage = async (payload, res) => {
+  const userId = payload.user.id;
+  const value = payload.actions[0].value;
+  const attachments = await getAttachments(userId, +value);
+
+  return res.json({
+    replace_original: true,
+    response_type: PRIVATE,
+    text: 'Select sticker',
+    attachments
+  });
+};
+
+const handleBtn = async function(req, res, next) {
+  const payload = JSON.parse(req.body.payload);
+  const actionName = payload.actions[0].name;
+
+  switch (actionName) {
+    case 'send_sticker': return sendSticker(payload, res);
+    case 'show_page': return showStickersPage(payload, res);
+    case 'remove_sticker': return removeSticker(payload, res);
+    case 'cancel': return cancelRequest(res);
+  }
 };
 
 module.exports = {
